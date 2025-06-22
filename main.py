4a1ca324a561a5273f983ec87f498970a0245453
Maciek Gajda <gajdam11@gmail.com>
2025-06-22 11:29:55 +0200

    flipping tasks completed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 import imutils
 
-image = cv2.imread('OIP.jpg')
+image = cv2.imread('tank.jpg')
 
 # ex 1
 flipped_hor = cv2.flip(image, 1)
@@ -26,4 +26,24 @@ cv2.imshow('flipped_both', flipped_both)
 cv2.waitKey(0)
 
 # ex 5
+(h, w) = image.shape[:2]
 
+startX, endX = w // 3, 2 * w // 3
+startY, endY = h // 3, 2 * h // 3
+roi = image[startY:endY, startX:endX]
+
+flipped_roi = cv2.flip(roi, 1)
+image[startY:endY, startX:endX] = flipped_roi
+cv2.imshow("Flipped ROI Image", image)
+cv2.waitKey(0)
+
+# ex 6
+
+flip_mode = int(input("Podaj sposób odbicia (0 – pionowe, 1 – poziome, -1 – oba): "))
+
+flipped = cv2.flip(image, flip_mode)
+
+cv2.imshow("Flipped Image", flipped)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
